9521e30e46e859a2248e7d6bddb25296fa476874
Simran Preet Singh <simran5590@gmail.com>
2022-01-10 07:04:20 -0800

    Initial Commit

---
+++ Crypto App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Crypto App
+//
+//  Created by Simran Preet Narang on 2022-01-10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Crypto App/Crypto_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Crypto_AppApp.swift
+//  Crypto App
+//
+//  Created by Simran Preet Narang on 2022-01-10.
+//
+
+import SwiftUI
+
+@main
+struct Crypto_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
