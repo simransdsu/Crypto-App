a587afb6990ace1dbb422f0443283c9612231f58
Simran Preet Singh <simran5590@gmail.com>
2022-02-11 07:37:07 -0800

    HACK: You cannot click on spacers in a ListRow to added a background.

--- Crypto App/Core/Home/Views/CoinRowView.swift
+++ Crypto App/Core/Home/Views/CoinRowView.swift
@@ -26,6 +26,9 @@ struct CoinRowView: View {
             rightColumn
         }
         .font(.subheadline)
+        .background(
+            Color.theme.background.opacity(0.001)
+        )
     }
 }
 
